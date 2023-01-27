188c322fbd8391a8a16c729852619185604cecd3
M.Haris Khan <hkhan@cedrus.com>
2023-01-27 15:05:48 +0500

    this is a new project

---
+++ assignment 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace assignment
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sellingp, costp, profit, loss;
+
+            Console.WriteLine("enter the cost price");
+            costp=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the seeling price");
+            Sellingp=Convert.ToInt32(Console.ReadLine());
+
+            if (Sellingp > costp)
+            {
+                profit = (Sellingp - costp);
+                Console.WriteLine("enter the profit = " + profit);
+
+
+
+            }
+
+            else if (Sellingp< costp)
+            {
+
+                loss= (Sellingp - costp);
+                Console.WriteLine("enter the loss price = " + loss);
+
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("dont get any loss or profit value");
+            }
+        }
+
+    }
+}
