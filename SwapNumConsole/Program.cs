19d17ab99397780f9e41802b434d109893965b7f
M.Haris Khan <hkhan@cedrus.com>
2023-01-27 15:15:28 +0500

    Swap Num Question C#

---
+++ SwapNumConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace ConsoleApp1
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("Enter the number for num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number for num2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("before swapping num1 = {0} and num2 = {1}", num1,num2);
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("after swapping num1 = {0} and num2 = {1}",num1,num2);
+            Console.ReadLine();
+
+
+
+
+        }
+        }
+    }
+
